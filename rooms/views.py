8d748698db742fc247268cffb49ffdfb2ffe855d
Eom Hyun Ho <uhh0701@gmail.com>
2019-12-14 22:39:34 +0900

    covert manual list_code to django's paginator

--- rooms/views.py
+++ rooms/views.py
@@ -1,28 +1,19 @@
-from math import ceil
-from django.shortcuts import render
-from django.core.paginator import Paginator
+from django.shortcuts import render, redirect
+from django.core.paginator import Paginator, EmptyPage
 from . import models
 
 
 def all_rooms(request):
     page = request.GET.get("page", 1)
-    page = int(page or 1)
-    page_size = 10
-    limit = page_size * page
-    offset = limit - page_size
+    room_list = (
+        models.Room.objects.all()
+    )  # Queryset : lazy (not load real data from db in this moment)
+    paginator = Paginator(room_list, per_page=10, orphans=5)
 
-    all_rooms = models.Room.objects.all()[
-        offset:limit
-    ]  # 다 불러온 후 slicing 하는 것이 아니라, 동시에 일어나는 사건
-    page_count = ceil(models.Room.objects.count() / page_size)
-
-    return render(
-        request,
-        "rooms/home.html",
-        context={
-            "rooms": all_rooms,
-            "page": page,
-            "page_count": page_count,
-            "page_range": range(0, page_count),
-        },
-    )
+    try:
+        # not have to handling exceptions but customizing less
+        # rooms = paginator.get_page(page)
+        rooms = paginator.page(int(page))
+        return render(request, "rooms/home.html", {"page": rooms})
+    except EmptyPage:
+        return redirect("/")
