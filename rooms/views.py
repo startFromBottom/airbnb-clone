fd246e7dd9936b4e2c41bff86dcb2eaea825db3d
Eom Hyun Ho <uhh0701@gmail.com>
2019-12-14 21:53:40 +0900

    add pagination function manually

--- rooms/views.py
+++ rooms/views.py
@@ -1,9 +1,28 @@
+from math import ceil
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from . import models
 
 
 def all_rooms(request):
+    page = request.GET.get("page", 1)
+    page = int(page or 1)
+    page_size = 10
+    limit = page_size * page
+    offset = limit - page_size
 
-    all_rooms = models.Room.objects.all()
+    all_rooms = models.Room.objects.all()[
+        offset:limit
+    ]  # 다 불러온 후 slicing 하는 것이 아니라, 동시에 일어나는 사건
+    page_count = ceil(models.Room.objects.count() / page_size)
 
-    return render(request, "rooms/home.html", context={"rooms": all_rooms})
+    return render(
+        request,
+        "rooms/home.html",
+        context={
+            "rooms": all_rooms,
+            "page": page,
+            "page_count": page_count,
+            "page_range": range(0, page_count),
+        },
+    )
