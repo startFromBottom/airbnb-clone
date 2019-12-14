65e1f9bf3e3aa9bdd1b8b6ee48a305090e3633bd
Eom Hyun Ho <uhh0701@gmail.com>
2019-12-14 21:10:07 +0900

    add urls.py and templates

---
+++ core/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from rooms import views as room_views
+
+app_name = "core"  # same with namespace in config -> urls.py
+
+urlpatterns = [path("", room_views.all_rooms, name="home")]
+
