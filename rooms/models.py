020c17ae21bcce209b951981d96a9305a44b66c5
Eom Hyun Ho <uhh0701@gmail.com>
2019-12-15 21:18:33 +0900

    add rooms detail view

--- rooms/models.py
+++ rooms/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from django_countries.fields import CountryField
 from core import models as core_models
 
@@ -90,12 +91,15 @@ class Room(core_models.TimeStampedModel):
     facilities = models.ManyToManyField("Facility", related_name="rooms", blank=True)
     house_rules = models.ManyToManyField("HouseRule", related_name="rooms", blank=True)
 
+    def __str__(self):
+        return self.name
+
     def save(self, *args, **kwargs):
         self.city = str.capitalize(self.city)
         super().save(*args, **kwargs)
 
-    def __str__(self):
-        return self.name
+    def get_absolute_url(self):
+        return reverse("rooms:detail", kwargs={"pk": self.pk})  # namespace:name
 
     def total_rating(self):
 
--- rooms/views.py
+++ rooms/views.py
@@ -1,5 +1,4 @@
-from django.utils import timezone
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from . import models
 
 
@@ -13,8 +12,19 @@ class HomeView(ListView):
     ordering = "created"
     context_object_name = "rooms"
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        now = timezone.now()
-        context["now"] = now
-        return context
+
+class RoomDetail(DetailView):
+
+    """ RoomDetail Definition """
+
+    # find value by pk
+
+    model = models.Room
+
+
+# def room_detail(request, pk):
+#     try:
+#         room = models.Room.objects.get(pk=pk)
+#         return render(request, "rooms/detail.html", context={"room": room})
+#     except models.Room.DoesNotExist:
+#         raise Http404()  # automatically render templates 404.html
