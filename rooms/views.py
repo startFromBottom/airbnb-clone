6a416e2dde0a54cd3053efb287f9603192fb9dd3
startFromBottom <uhh0701@gmail.com>
2019-12-17 22:56:17 +0900

    In room apps's views.py -> make form logic manually

--- rooms/views.py
+++ rooms/views.py
@@ -43,11 +43,10 @@ def search(request):
     bedrooms = int(request.GET.get("bedrooms", 0))
     beds = int(request.GET.get("beds", 0))
     baths = int(request.GET.get("baths", 0))
-    instant = request.GET.get("instant", False)
-    super_host = request.GET.get("super_host", False)
+    instant = bool(request.GET.get("instant", False))
+    superhost = bool(request.GET.get("superhost", False))
     s_amenities = request.GET.getlist("amenities")
     s_facilities = request.GET.getlist("facilities")
-    print(s_amenities, s_facilities)
 
     form = {
         "city": city,
@@ -59,7 +58,7 @@ def search(request):
         "beds": beds,
         "baths": baths,
         "instant": instant,
-        "super_host": super_host,
+        "superhost": superhost,
         "s_amenities": s_amenities,
         "s_facilities": s_facilities,
     }
@@ -75,4 +74,47 @@ def search(request):
         "facilities": facilities,
     }
 
-    return render(request, "rooms/search.html", {**form, **choices})
+    filter_args = {}
+
+    if city != "Anywhere":
+        filter_args["city__startswith"] = city
+
+    filter_args["country"] = country
+
+    if room_type != 0:
+        filter_args["room_type__pk"] = room_type  # room_type : foreign key
+
+    if price != 0:
+        filter_args["price__lte"] = price
+
+    if guests != 0:
+        filter_args["guests__gte"] = guests
+
+    if bedrooms != 0:
+        filter_args["bedrooms__gte"] = bedrooms
+
+    if beds != 0:
+        filter_args["beds__gte"] = bedrooms
+
+    if baths != 0:
+        filter_args["baths__gte"] = baths
+
+    if instant is True:
+        filter_args["instant_book"] = True
+
+    if superhost is True:
+        filter_args["host__superhost"] = True
+
+    rooms = models.Room.objects.filter(**filter_args)
+
+    if len(s_amenities) > 0:
+        for s_amenity in s_amenities:
+            rooms = rooms.filter(amenities__pk=int(s_amenity))
+
+    if len(s_facilities) > 0:
+        for s_facility in s_facilities:
+            rooms = rooms.filter(facilities__pk=int(s_facility))
+
+    rooms = models.Room.objects.filter(**filter_args)
+
+    return render(request, "rooms/search.html", {**form, **choices, "rooms": rooms})
