54304d00c6e848db9d6373584126cf4917cbb5f4
startFromBottom <uhh0701@gmail.com>
2019-12-18 23:32:52 +0900

    implement Validating Email and Password

--- users/views.py
+++ users/views.py
@@ -1,3 +1,16 @@
+from django.views import View
 from django.shortcuts import render
+from . import forms
 
-# Create your views here.
+
+class LoginView(View):
+    def get(self, request):
+        form = forms.LoginForm(initial={"email": "itn@las.com"})
+        return render(request, "users/login.html", {"form": form})
+
+    def post(self, request):
+        form = forms.LoginForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)  # should return in forms's method
+        return render(request, "users/login.html", {"form": form})
+
