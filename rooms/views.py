e9b467ad8d0cd79f7e102eb75a4c821434cb894d
startFromBottom <uhh0701@gmail.com>
2019-12-18 14:33:20 +0900

    implement form part by using django-form

--- rooms/views.py
+++ rooms/views.py
@@ -1,7 +1,10 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, View
 from django.shortcuts import render
+from django.core.paginator import Paginator
+from django.db.models import Q
 from django_countries import countries
 from . import models
+from . import forms
 
 
 class HomeView(ListView):
@@ -24,97 +27,79 @@ class RoomDetail(DetailView):
     model = models.Room
 
 
-# def room_detail(request, pk):
-#     try:
-#         room = models.Room.objects.get(pk=pk)
-#         return render(request, "rooms/detail.html", context={"room": room})
-#     except models.Room.DoesNotExist:
-#         raise Http404()  # automatically render templates 404.html
+class SearchView(View):
+    def get(self, request):
 
+        country = request.GET.get("country")
 
-def search(request):
+        if country:
 
-    city = request.GET.get("city", "Anywhere")
-    city = str.capitalize(city)
-    country = request.GET.get("country", "KR")
-    room_type = int(request.GET.get("room_type", 0))
-    price = int(request.GET.get("price", 0))
-    guests = int(request.GET.get("guests", 0))
-    bedrooms = int(request.GET.get("bedrooms", 0))
-    beds = int(request.GET.get("beds", 0))
-    baths = int(request.GET.get("baths", 0))
-    instant = bool(request.GET.get("instant", False))
-    superhost = bool(request.GET.get("superhost", False))
-    s_amenities = request.GET.getlist("amenities")
-    s_facilities = request.GET.getlist("facilities")
+            form = forms.SearchForm(request.GET)
 
-    form = {
-        "city": city,
-        "s_room_type": room_type,
-        "s_country": country,
-        "price": price,
-        "guests": guests,
-        "bedrooms": bedrooms,
-        "beds": beds,
-        "baths": baths,
-        "instant": instant,
-        "superhost": superhost,
-        "s_amenities": s_amenities,
-        "s_facilities": s_facilities,
-    }
+            if form.is_valid():
 
-    room_types = models.RoomType.objects.all()
-    amenities = models.Amenity.objects.all()
-    facilities = models.Facility.objects.all()
+                city = form.cleaned_data.get("city")
+                country = form.cleaned_data.get("country")
+                room_type = form.cleaned_data.get("room_type")
+                price = form.cleaned_data.get("price")
+                guests = form.cleaned_data.get("guests")
+                bedrooms = form.cleaned_data.get("bedrooms")
+                beds = form.cleaned_data.get("beds")
+                baths = form.cleaned_data.get("baths")
+                instant_book = form.cleaned_data.get("instant_book")
+                superhost = form.cleaned_data.get("superhost")
+                # Multiple Choice Field
+                amenities = form.cleaned_data.get("amenities")
+                facilities = form.cleaned_data.get("facilities")
 
-    choices = {
-        "countries": countries,
-        "room_types": room_types,
-        "amenities": amenities,
-        "facilities": facilities,
-    }
+                queries = []
 
-    filter_args = {}
+                if city != "Anywhere":
+                    queries.append(Q(city__startswith=city))
 
-    if city != "Anywhere":
-        filter_args["city__startswith"] = city
+                queries.append(Q(country=country))
 
-    filter_args["country"] = country
+                if room_type is not None:
+                    queries.append(Q(room_type=room_type))
 
-    if room_type != 0:
-        filter_args["room_type__pk"] = room_type  # room_type : foreign key
+                if price is not None:
+                    queries.append(Q(price__lte=price))
 
-    if price != 0:
-        filter_args["price__lte"] = price
+                if guests is not None:
+                    queries.append(Q(guests__gte=guests))
 
-    if guests != 0:
-        filter_args["guests__gte"] = guests
+                if bedrooms is not None:
+                    queries.append(Q(bedrooms__gte=bedrooms))
 
-    if bedrooms != 0:
-        filter_args["bedrooms__gte"] = bedrooms
+                if beds is not None:
+                    queries.append(Q(beds__gte=beds))
 
-    if beds != 0:
-        filter_args["beds__gte"] = bedrooms
+                if baths is not None:
+                    queries.append(Q(baths__gte=baths))
 
-    if baths != 0:
-        filter_args["baths__gte"] = baths
+                if instant_book is True:
+                    queries.append(Q(instant_book=True))
 
-    if instant is True:
-        filter_args["instant_book"] = True
+                if superhost is True:
+                    queries.append(Q(host__superhost=True))
 
-    if superhost is True:
-        filter_args["host__superhost"] = True
+                for amenity in amenities:
+                    queries.append(Q(amenities__name=amenity.name))
 
-    rooms = models.Room.objects.filter(**filter_args)
+                for facility in facilities:
+                    queries.append(Q(facilities__name=facility.name))
 
-    if len(s_amenities) > 0:
-        for s_amenity in s_amenities:
-            rooms = rooms.filter(amenities__pk=int(s_amenity))
+                qs = models.Room.objects.filter(*queries).order_by("-created")
 
-    if len(s_facilities) > 0:
-        for s_facility in s_facilities:
-            rooms = rooms.filter(facilities__pk=int(s_facility))
+                paginator = Paginator(qs, 10, orphans=5)
+                page = request.GET.get("page", 1)
+                rooms = paginator.get_page(page)
 
-    rooms = models.Room.objects.filter(**filter_args)
+                return render(
+                    request, "rooms/search.html", {"form": form, "rooms": rooms}
+                )
 
-    return render(request, "rooms/search.html", {**form, **choices, "rooms": rooms})
+        else:
+            form = forms.SearchForm()  # empty form without validation
+
+        return render(request, "rooms/search.html", {"form": form})
