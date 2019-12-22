1fa8fbd12050ed6b61400d8a825657ea3ef4388c
startFromBottom <uhh0701@gmail.com>
2019-12-22 15:19:14 +0900

    add gulp, tailwind css settings

--- config/settings.py
+++ config/settings.py
@@ -126,7 +126,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "/static/"  # server url, not directory
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 AUTH_USER_MODEL = "users.User"
 
