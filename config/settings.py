7fd0a3c70ee53978dd7cb4c042ad5dac0d260791
startFromBottom <uhh0701@gmail.com>
2019-12-31 23:41:34 +0900

    update settings.py

--- config/settings.py
+++ config/settings.py
@@ -185,7 +185,7 @@ if DEBUG is False:
     AWS_STORAGE_BUCKET_NAME = "airbnb-clone-eom123"
     AWS_AUTO_CREATE_BUCKET = True
     AWS_BUCKET_ACL = "public-read"
-
+    AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
     AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
     STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"
 
